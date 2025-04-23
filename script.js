41a70013528281e24392f01229bcf1116e74bbef
Leonardo-Davio <leonardo.davio@edu.unifi.it>
2025-04-23 18:36:52 +0200

    Header Done to create the header with also the resize for mobile

--- script.js
+++ script.js
@@ -6,3 +6,38 @@ function toggleVisibility(conId) {
         container.style.display = 'block';
     }
 }
+function toggleVisibilityMultiId(ids){
+    ids.forEach(conId => {
+        toggleVisibility(conId);
+    });
+}
+
+const MENUID =["Industry", "Partner", "Events", "Marketing", "Newsletters"]
+
+function toggleVisibilityMenu(conId) {/*TODO finserire la transizione*/
+    const containerSelect = document.getElementById(conId);
+    if (containerSelect.style.display === 'block') {
+        containerSelect.style.display = 'none';
+    } else {
+    MENUID.forEach(id => {
+            const container = document.getElementById(id);
+            container.style.display = 'none';
+        })
+        containerSelect.style.display = 'block';
+    }
+}
+
+const MENUID12 =["Industry12", "Partner12", "Events12", "Marketing12", "Newsletters12"]
+
+function toggleVisibilityMenu12(conId) {
+    const containerSelect = document.getElementById(conId);
+    if (containerSelect.style.display === 'block') {
+        containerSelect.style.display = 'none';
+    } else {
+    MENUID12.forEach(id => {
+            const container = document.getElementById(id);
+            container.style.display = 'none';
+        })
+        containerSelect.style.display = 'block';
+    }
+}
